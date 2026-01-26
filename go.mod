c86067bf364e3e290ea11e1a6da86e2cc98d6373
sideef5ect <imaury2020@gmail.com>
2026-01-26 15:23:50 +0800

    feat: add ws package re-exporting centrifuge-go SDK

--- go.mod
+++ go.mod
@@ -2,11 +2,26 @@ module github.com/cyberapper/cadenza-client-go
 
 go 1.23
 
-require gopkg.in/validator.v2 v2.0.1
+require (
+	github.com/centrifugal/centrifuge-go v0.10.11
+	github.com/stretchr/testify v1.11.1
+	gopkg.in/validator.v2 v2.0.1
+)
 
 require (
+	github.com/centrifugal/protocol v0.16.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/planetscale/vtprotobuf v0.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.11.1 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/segmentio/encoding v0.4.0 // indirect
+	github.com/shadowspore/fossil-delta v0.0.0-20241213113458-1d797d70cbe3 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ ws/centrifugo.go
@@ -0,0 +1,144 @@
+// Package ws re-exports the Centrifugo client SDK for WebSocket connections.
+//
+// Usage:
+//
+//	import "github.com/cyberapper/cadenza-client-go/ws"
+//
+//	client := ws.NewJsonClient(wsURL, ws.Config{Token: token})
+//	client.Connect()
+package ws
+
+import "github.com/centrifugal/centrifuge-go"
+
+// Client and configuration types
+type (
+	Client             = centrifuge.Client
+	Config             = centrifuge.Config
+	Subscription       = centrifuge.Subscription
+	SubscriptionConfig = centrifuge.SubscriptionConfig
+	ClientInfo         = centrifuge.ClientInfo
+	StreamPosition     = centrifuge.StreamPosition
+	Publication        = centrifuge.Publication
+)
+
+// Event types
+type (
+	PublicationEvent        = centrifuge.PublicationEvent
+	JoinEvent               = centrifuge.JoinEvent
+	LeaveEvent              = centrifuge.LeaveEvent
+	SubscribedEvent         = centrifuge.SubscribedEvent
+	UnsubscribedEvent       = centrifuge.UnsubscribedEvent
+	SubscribingEvent        = centrifuge.SubscribingEvent
+	SubscriptionErrorEvent  = centrifuge.SubscriptionErrorEvent
+	SubscriptionTokenEvent  = centrifuge.SubscriptionTokenEvent
+	ConnectedEvent          = centrifuge.ConnectedEvent
+	ConnectingEvent         = centrifuge.ConnectingEvent
+	DisconnectedEvent       = centrifuge.DisconnectedEvent
+	ErrorEvent              = centrifuge.ErrorEvent
+	MessageEvent            = centrifuge.MessageEvent
+	ConnectionTokenEvent    = centrifuge.ConnectionTokenEvent
+	ServerPublicationEvent  = centrifuge.ServerPublicationEvent
+	ServerJoinEvent         = centrifuge.ServerJoinEvent
+	ServerLeaveEvent        = centrifuge.ServerLeaveEvent
+	ServerSubscribedEvent   = centrifuge.ServerSubscribedEvent
+	ServerUnsubscribedEvent = centrifuge.ServerUnsubscribedEvent
+	ServerSubscribingEvent  = centrifuge.ServerSubscribingEvent
+)
+
+// Handler types
+type (
+	PublicationHandler          = centrifuge.PublicationHandler
+	JoinHandler                 = centrifuge.JoinHandler
+	LeaveHandler                = centrifuge.LeaveHandler
+	SubscribedHandler           = centrifuge.SubscribedHandler
+	UnsubscribedHandler         = centrifuge.UnsubscribedHandler
+	SubscribingHandler          = centrifuge.SubscribingHandler
+	SubscriptionErrorHandler    = centrifuge.SubscriptionErrorHandler
+	ConnectedHandler            = centrifuge.ConnectedHandler
+	ConnectingHandler           = centrifuge.ConnectingHandler
+	DisconnectHandler           = centrifuge.DisconnectHandler
+	ErrorHandler                = centrifuge.ErrorHandler
+	MessageHandler              = centrifuge.MessageHandler
+	ServerPublicationHandler    = centrifuge.ServerPublicationHandler
+	ServerJoinHandler           = centrifuge.ServerJoinHandler
+	ServerLeaveHandler          = centrifuge.ServerLeaveHandler
+	ServerSubscribedHandler     = centrifuge.ServerSubscribedHandler
+	ServerUnsubscribedHandler   = centrifuge.ServerUnsubscribedHandler
+	ServerSubscribingHandler    = centrifuge.ServerSubscribingHandler
+	LogHandler                  = centrifuge.LogHandler
+)
+
+// Result types
+type (
+	RPCResult           = centrifuge.RPCResult
+	PublishResult       = centrifuge.PublishResult
+	HistoryResult       = centrifuge.HistoryResult
+	PresenceResult      = centrifuge.PresenceResult
+	PresenceStatsResult = centrifuge.PresenceStatsResult
+	PresenceStats       = centrifuge.PresenceStats
+	UnsubscribeResult   = centrifuge.UnsubscribeResult
+)
+
+// Error types
+type (
+	Error                        = centrifuge.Error
+	ConfigurationError           = centrifuge.ConfigurationError
+	ConnectError                 = centrifuge.ConnectError
+	RefreshError                 = centrifuge.RefreshError
+	TransportError               = centrifuge.TransportError
+	SubscriptionRefreshError     = centrifuge.SubscriptionRefreshError
+	SubscriptionSubscribeError   = centrifuge.SubscriptionSubscribeError
+)
+
+// Other types
+type (
+	LogEntry  = centrifuge.LogEntry
+	LogLevel  = centrifuge.LogLevel
+	State     = centrifuge.State
+	SubState  = centrifuge.SubState
+	DeltaType = centrifuge.DeltaType
+	HistoryOption  = centrifuge.HistoryOption
+	HistoryOptions = centrifuge.HistoryOptions
+)
+
+// Constructors
+var (
+	NewJsonClient     = centrifuge.NewJsonClient
+	NewProtobufClient = centrifuge.NewProtobufClient
+)
+
+// History options
+var (
+	WithHistoryLimit   = centrifuge.WithHistoryLimit
+	WithHistoryReverse = centrifuge.WithHistoryReverse
+	WithHistorySince   = centrifuge.WithHistorySince
+)
+
+// Constants
+const (
+	LogLevelNone  = centrifuge.LogLevelNone
+	LogLevelDebug = centrifuge.LogLevelDebug
+	LogLevelTrace = centrifuge.LogLevelTrace
+)
+
+const (
+	StateDisconnected = centrifuge.StateDisconnected
+	StateConnecting   = centrifuge.StateConnecting
+	StateConnected    = centrifuge.StateConnected
+)
+
+const (
+	SubStateUnsubscribed = centrifuge.SubStateUnsubscribed
+	SubStateSubscribing  = centrifuge.SubStateSubscribing
+	SubStateSubscribed   = centrifuge.SubStateSubscribed
+)
+
+const (
+	DeltaTypeNone   = centrifuge.DeltaTypeNone
+	DeltaTypeFossil = centrifuge.DeltaTypeFossil
+)
+
+// Errors
+var (
+	ErrTimeout = centrifuge.ErrTimeout
+)
